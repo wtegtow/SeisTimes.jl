e76d5bed28ddbdfd6d045ef0f8138db334c0da8f
Documenter.jl <documenter@juliadocs.github.io>
2025-07-24 19:38:08 +0000

    build based on 11596f6

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SeisTimes","page":"Home","title":"SeisTimes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SeisTimes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
