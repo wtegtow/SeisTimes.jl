0496f839720663f2f6d2f2c1be81ddd1723a2d8e
Documenter.jl <documenter@juliadocs.github.io>
2026-03-03 10:22:11 +0000

    build based on 3ee2048

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SeisTimes","page":"Home","title":"SeisTimes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SeisTimes.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#SeisTimes","page":"Home","title":"SeisTimes","text":"Documentation for SeisTimes.\n\n","category":"section"}]
 }
